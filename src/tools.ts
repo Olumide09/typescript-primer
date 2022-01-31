a36eb338ded38f5faa5de44b4011b978508709c9
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-01-31 16:51:00 +0100

    Update tools.ts

--- src/tools.ts
+++ src/tools.ts
@@ -2,12 +2,12 @@ export class Person {
     //declare data type in TS
     firstName: string;
     lastName: string;
-    gender: string;
+    gender: "male" | "female";
     height: number;
     hobbies: string[];
     birthday: Date;
     isAlive: boolean;
-    constructor(firstName, lastName, gender, height, hobbies, birthday, isAlive) {
+    constructor(firstName: string, lastName: string, gender: "male" | "female", height: number, hobbies: string[], birthday: Date, isAlive: boolean) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
