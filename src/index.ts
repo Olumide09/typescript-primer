b3ac98570857454030115efa51895794fe1de9ee
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-02-02 09:42:50 +0100

    update tools and index

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ name = 'Paul';
 console.log(name)
 count = 5
 
-import logger, {Person} from './tools'
+import logger, {IPersonData, Person} from './tools'
 
-const person1 = new Person(
+/*const person1 = new Person(
     "Fathia"
     ,"Saludeen",
      "female",
@@ -19,7 +19,17 @@ const person1 = new Person(
        new Date("2002-01-09"),
         true);
 const pronoun = person1.gender === "female"? "She" : "He";
+*/
  
+let person1Data: IPersonData = {
+    firstName: "Pius",
+    lastName: "Onobhayedo",
+    gender: "male",
+    height: 1.5,
+    hobbies: [],
+    birthday: new Date(1900,10,10),
+    isAlive: true
+   }
+const person1 = new Person(person1Data);
 
-
-    logger(`The first person is ${person1.firstName} ${person1.lastName}, ${person1.gender === "female"? "She":"He"} likes ${person1.hobbies.join(",")}. ${person1.birthday.toLocaleDateString}`)    
+    logger(`The first person is ${person1Data.firstName} ${person1Data.lastName}, ${person1Data.gender === "female"? "She":"He"} likes ${person1Data.hobbies.join(",")}. ${person1Data.birthday.toLocaleDateString}`)    
--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,17 @@
+export interface IPersonData {
+    firstName: string,
+    lastName: string,
+    gender: "male" | "female",
+    height?: number, //question mark here means optional
+    hobbies: string[],
+    birthday?: Date, //question mark here means optional
+    isAlive: boolean
+}
+
+
 export class Person {
     //declare data type in TS
+    // class Person definition starts here.
     firstName: string;
     lastName: string;
     gender: "male" | "female";
@@ -7,19 +19,35 @@ export class Person {
     hobbies: string[];
     birthday: Date;
     isAlive: boolean;
-    constructor(firstName: string, lastName: string, gender: "male" | "female", height: number, hobbies: string[], birthday: Date, isAlive: boolean) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.height = height;
-        this.hobbies = hobbies;
-        this.birthday = birthday;
-        this.isAlive = isAlive;
-    }
-};
 
+
+/*  constructor(firstName: string, lastName: string, gender: "male" | "female", height: number, hobbies: string[], birthday: Date, isAlive: boolean) {
+       this.firstName = firstName;
+       this.lastName = lastName;
+       this.gender = gender;
+       this.height = height;
+       this.hobbies = hobbies;
+       this.birthday = birthday;
+       this.isAlive = isAlive;
+   }
+};
+*/
+/** Section on interface and classes. */
+    constructor(personData: IPersonData) {
+        this.firstName = personData.firstName;
+        this.lastName = personData.lastName;
+        this.gender = personData.gender;
+        this.height = personData.height;
+        this.hobbies = personData.hobbies;
+        this.birthday = personData.birthday;
+        this.isAlive = personData.isAlive;
+}
+
+}
 //interface is a class without methods.
 // class contains properties and methods.
+
+
 
 
 export const logger = (output: string) => {
